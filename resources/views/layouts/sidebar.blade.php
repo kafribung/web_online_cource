b82eb46690a272544fc19ab44b3f6bcab29ba7af
kafribung <60900117001@uin-alauddin.ac.id>
2021-02-21 08:11:41 +0800

    spatie role permission

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,8 +1,10 @@
 <div class="md:w-1/6 bg-white min-h-screen px-4">
+    @role('super-admin')
     <div class="mb-5">
         <h3 class="text-base text-gray-800 font-medium py-2 uppercase">Dashboard</h3>
         <a href="#" class="text-sm text-gray-500 hover:text-gray-400 py-2">Dashboard</a>
     </div>
+    
     <div class="mb-5">
         <h3 class="text-base text-gray-800 font-medium py-2 uppercase">Admin</h3>
         <a href="#" class="text-sm text-gray-500 hover:text-gray-400 py-2">Admin</a>
@@ -11,12 +13,20 @@
         <h3 class="text-base text-gray-800 font-medium py-2 uppercase">Users</h3>
         <a href="#" class="text-sm text-gray-500 hover:text-gray-400 py-2">User</a>
     </div>
+    @endrole
+
+    {{-- Gate::before super-admin hanya dapat mendeteksi method can --}}
+    {{-- Admin : create playlist|create playlist --}}
+    @can('create playlist')
     <div class="mb-5">
         <h3 class="text-base text-gray-800 font-medium py-2 uppercase">Playlist</h3>
         <a href="#" class="text-sm text-gray-500 hover:text-gray-400 py-2">Playlist</a>
     </div>
+    @endcan
+    @if (Auth::user()->can('create category'))
     <div class="mb-5">
         <h3 class="text-base text-gray-800 font-medium py-2 uppercase">Category</h3>
         <a href="#" class="text-sm text-gray-500 hover:text-gray-400 py-2">Category</a>
     </div>
+    @endif
 </div>
