d7a61a2dde0ef9ee1d69f5c594f9354f70c891fd
kafribung <60900117001@uin-alauddin.ac.id>
2021-03-19 21:48:34 +0800

    hover icon edit & delete

--- resources/views/backend/playlist/playlist.blade.php
+++ resources/views/backend/playlist/playlist.blade.php
@@ -20,8 +20,21 @@
                 </div>
                 
                 <div class="m-2 text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis, optio?</div> 
-                <div class="flex">
-                    <button class="bg-blue-400 px-4 py-1 rounded-xl outline-none focus:outline-none  hover:bg-blue-300">Sapi</button>
+                <div class="flex justify-center mb-2">
+                    <a href="" class="border-2 border-yellow-200 rounded-md p-1 mr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" class="h-4 w-4 text-yellow-500 hover:text-yellow-300">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                        </svg>
+                    </a>
+                    <button  id="delete" onclick=" deleteAdmin()"  class="border-2 border-red-200 rounded-md p-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" class="h-4 w-4 text-red-500 hover:text-red-300">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                    </button>
                 </div>
             </div>
         </div>
